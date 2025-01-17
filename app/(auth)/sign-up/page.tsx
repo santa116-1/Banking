b594d26489b870da0bc91a95074991e0eb892c65
santa116-1 <totoplife116@gmail.com>
2025-01-17 10:37:59 +0200

    feat: initial commit

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SignUphttp://localhost:3000/ = () => {
+  return (
+    <div>SignUphttp://localhost:3000/</div>
+  )
+}
+
+export default SignUphttp://localhost:3000/
